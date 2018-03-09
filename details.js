cd08e03ad60b1bdd46ea246993ec723853f50afd
ilmarika <ilmari.linna@gmail.com>
2018-03-09 14:16:09 +0200

    js for movie details page

---
+++ details.js
@@ -0,0 +1,52 @@
+let url = new URL($(location).attr('href'));
+let id = url.searchParams.get('id');
+console.log(id);
+
+//Setting up the db connection
+let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": 'https://leffakanta-da2e.restdb.io/rest/leffat?q={"_id":'+ '"'+id+'"}',
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "5a9e49463f8df0b91f627ce5",
+      "cache-control": "no-cache"
+    }
+}
+$.ajax(settings).done(function (response) {
+  console.log(response[0]);
+  console.log(response[0].Title);
+  // Create elements for the movie details to show in the webpage, add text to elements and append them to the main element.
+  if (response[0].Title == undefined) {
+    let notfound = document.createElement('div');
+    notfound.setAttribute('class', 'alert alert-danger');
+    notfound.innerHTML = 'Title not found!'
+    movie.appendChild(notfound);
+  } else {
+    let title = document.createElement("div");
+    let released = document.createElement("div");
+    let country = document.createElement("div");
+    let genre = document.createElement("div");
+    let synopsis = document.createElement('div');
+    title.setAttribute("id", 'Title');
+    released.setAttribute("id", 'Released');
+    country.setAttribute("id", 'Country');
+    genre.setAttribute("id", 'Genre');
+    synopsis.setAttribute('id', 'Synopsis');
+    title.innerHTML = 'Title: ' + response[0].Title;
+    released.innerHTML = 'Release date: ' + response[0].Released;
+    country.innerHTML = 'Country: ' + response[0].Country;
+    genre.innerHTML = 'Genre: ' + response[0].Genre;
+    synopsis.innerHTML = 'Synopsis: ' + response[0].Plot;
+    var poster = document.getElementById('poster');
+    poster.innerHTML = "<img src=" + response[0].Poster + "></img>";
+    movie.appendChild(title);
+    movie.appendChild(released);
+    movie.appendChild(country);
+    movie.appendChild(genre);
+    movie.appendChild(synopsis);
+  }
+
+});
+
