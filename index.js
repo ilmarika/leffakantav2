dd6e0f145b70ed0181c78cb0f8c3495f02798e37
ilmarika <ilmari.linna@gmail.com>
2018-03-09 15:37:03 +0200

    Frontpage for the db

---
+++ index.js
@@ -0,0 +1,8 @@
+//when user clicks the submit button -> redirected to search page.
+$( '#submit' ).click(function() {
+    window.location.assign('list.html?q='+document.getElementById("search").value)
+})
+
+$( '#all' ).click(function() {
+    window.location.assign('list.html?q=')
+})
